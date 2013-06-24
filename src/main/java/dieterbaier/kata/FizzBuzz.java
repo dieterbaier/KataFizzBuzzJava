158e5422f811a06abcaa70fd962dc55e5d1cdf87
dbaier <dieter.baier@novatec-gmbh.de>
2013-06-24 14:54:45 +0200

    Counting words

---
+++ src/main/java/dieterbaier/kata/FizzBuzz.java
@@ -0,0 +1,38 @@
+package dieterbaier.kata;
+
+import java.io.PrintStream;
+
+public class FizzBuzz {
+
+  public void print(final int start, final int end, final PrintStream out) {
+    for (int i = start; i <= end; i++)
+      out.println(convert(i));
+  }
+
+  private String convert(final int number) {
+    if (isFizzBuzzCandidate(number))
+      return "FizzBuzz";
+    if (isBuzzKandidate(number))
+      return "Buzz";
+    if (isFizzKandidate(number))
+      return "Fizz";
+    return Integer.toString(number);
+  }
+
+  private boolean isBuzzKandidate(final int number) {
+    return isNumberDevisibleByDigit(number, 5) || Integer.toString(number).contains("5");
+  }
+
+  private boolean isFizzBuzzCandidate(final int number) {
+    return isNumberDevisibleByDigit(number, 5) && isNumberDevisibleByDigit(number, 3);
+  }
+
+  private boolean isFizzKandidate(final int number) {
+    return isNumberDevisibleByDigit(number, 3) || Integer.toString(number).contains("3");
+  }
+
+  private boolean isNumberDevisibleByDigit(final int number, final int digit) {
+    return number % digit == 0;
+  }
+
+}
--- src/test/java/dieterbaier/kata/FizzBuzzTest.java
+++ src/test/java/dieterbaier/kata/FizzBuzzTest.java
@@ -1,42 +1,55 @@
 package dieterbaier.kata;
 
-import static org.hamcrest.MatcherAssert.*;
-import static org.hamcrest.Matchers.*;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.util.StringTokenizer;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
-import dieterbaier.kata.fizzbuzz.FizzBuzz;
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.testng.annotations.Test;
 
 public class FizzBuzzTest {
-  
+
   @Test
-  public void PrintNumbers() throws IOException {
-    OutputStream bof = new ByteArrayOutputStream();
-    new FizzBuzz().print(1,100, bof);
-    assertThat("Invalid amount of numbers printed",countPrintedNumbers(bof.toString()), is(100));
-  }
-  
-  @Test
-  public void PrintNegativeNumbers() throws IOException {
-    OutputStream bof = new ByteArrayOutputStream();
-    new FizzBuzz().print(-100,-1,bof);
-    assertThat("Invalid amount of numbers printed",countPrintedNumbers(bof.toString()), is(100));
-  }
-  
-  @Test
-  public void TryToPrintWithStartBiggerThanEnd() throws IOException {
-    OutputStream bof = new ByteArrayOutputStream();
-    new FizzBuzz().print(100,1,bof);
-    assertThat("Invalid amount of numbers printed",countPrintedNumbers(bof.toString()), is(0));
-  }
-  
-  private int countPrintedNumbers(String string) {
-    StringTokenizer st = new StringTokenizer(string, System.getProperty("line.separator"));
-    return st.countTokens();
+  public void CountBuzz() {
+    assertThat(count(1, 100, "Buzz"), is(22));
   }
 
+  @Test
+  public void CountFizz() {
+    assertThat(count(1, 100, "Fizz"), is(34));
+  }
+
+  @Test
+  public void CountFizzBuzz() {
+    assertThat(count(1, 100, "FizzBuzz"), is(6));
+  }
+
+  @Test
+  public void CountLines() {
+    assertThat(count(1, 100, null), is(100));
+  }
+
+  @Test
+  public void PrintLines() {
+    new FizzBuzz().print(1, 100, System.out);
+  }
+
+  private Integer count(final int start, final int end, final String word) {
+    final OutputStream os = new ByteArrayOutputStream();
+    new FizzBuzz().print(start, end, new PrintStream(os));
+    String pattern;
+    if (word == null)
+      pattern = "[/d]*" + System.getProperty("line.separator");
+    else
+      pattern = "\\b" + word + "\\b";
+    final Matcher matcher = Pattern.compile(pattern).matcher(os.toString());
+    int count = 0;
+    while (matcher.find())
+      count++;
+    return count;
+  }
 }
